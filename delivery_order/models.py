0673b5c9b3183121363b130ee84cce8b1972b409
Dima <hovorovskiid@gmail.com>
2023-07-27 19:21:34 +0300

    Add models

--- delivery_order/models.py
+++ delivery_order/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from user.models import User
+from menu.models import MenuItem
 
-# Create your models here.
+
+class DeliveryOrder(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    items = models.ManyToManyField(MenuItem)
+    total_price = models.IntegerField()
+    delivery_address = models.CharField(max_length=255)
