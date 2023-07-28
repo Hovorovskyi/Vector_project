bf9dcaff4fa6cac7f598274e60e8d4725a6c018b
Dima <hovorovskiid@gmail.com>
2023-07-28 20:19:23 +0300

    Append views for user and menu. Connect user urls and menu urls

--- menu/views.py
+++ menu/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+from .models import MenuItem
 
-# Create your views here.
+
+class MenuItemView(ListView):
+    model = MenuItem
+    template_name = 'menu/menu_list.html'
+    context_object_name = 'menu_items'
+    allow_empty = False
+
+    def get_queryset(self):
+        return MenuItem.objects.filter(category=self.kwargs['category'])
